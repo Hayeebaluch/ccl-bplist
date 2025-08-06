097d5c646d47a65f5e94fb323517620373350db3
Hayeebaluch <hayeebaluch@icloud.com>
2025-08-06 04:57:54 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='ccl_bplist',
+    version='1.0.0',
+    py_modules=['ccl_bplist'],
+    description='Binary plist and NSKeyedArchiver parser',
+    author='Hayeebaluch',
+    url='https://github.com/Hayeebaluch/ccl-bplist',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Operating System :: OS Independent',
+    ],
+)
+
+
